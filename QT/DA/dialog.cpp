5eabcd132e0df18ab02aa0cf1813dd564290cf34
Natasha Singh <natasha.iwscope@gmail.com>
2017-12-24 01:17:17 -0500

    Sun Dec 24 01:17:17 EST 2017

--- QT/DA/dialog.cpp
+++ QT/DA/dialog.cpp
@@ -2,34 +2,9 @@
 
 #include "dialog.h"
 
-Dialog::Dialog(QWidget *parent, bool smallScreen)
-    : QDialog(parent)
+Dialog::Dialog(QWidget *parent, bool smallScreen) : QDialog(parent),ui(new Ui::Dialog)
 {
     QLineEdit *lineEdit = new QLineEdit;
+    ui->setupUi(this);
 
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->addWidget(lineEdit);
-    setLayout(layout);
-
-#ifdef QT_SOFTKEYS_ENABLED
-    QAction *exitAction = new QAction(tr("Exit"), this);
-    exitAction->setSoftKeyRole(QAction::NegativeSoftKey);
-    connect (exitAction, SIGNAL(triggered()), this, SLOT(close()));
-    addAction (exitAction);
-
-    Qt::WindowFlags flags = windowFlags();
-    flags |= Qt::WindowSoftkeysVisibleHint;
-    setWindowFlags(flags);
-#endif
-
-    if (!smallScreen)
-    {
-        lineEdit->setText(tr("Hello world!"));
-    }
-    else
-    {
-        lineEdit->setText(tr("Hello!"));
-    }
-    setWindowTitle(tr("Wiggly"));
-    resize(360, 145);
 }
--- QT/DA/dialog.h
+++ QT/DA/dialog.h
@@ -2,12 +2,20 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include "ui_display.h"
+
+namespace Ui
+{
+class Dialog;
+}
+
 
 class Dialog : public QDialog
 {
     Q_OBJECT
 
 public:
+    Ui::Dialog *ui;
     Dialog(QWidget *parent = 0, bool smallScreen = false);
 };
 
