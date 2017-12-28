790981f20a570d051e5ddcb25162246d5560c9e1
Natasha Singh <natasha.iwscope@gmail.com>
2017-12-27 23:38:36 -0500

    Wed, Dec 27, 2017 11:38:36 PM

--- lib/include/ext_udp.h
+++ lib/include/ext_udp.h
@@ -307,7 +307,10 @@ PREFIX unsigned short usWsHWEmulate();
 PREFIX unsigned short usWsHWEmulateCard(unsigned long i, unsigned long j);
 PREFIX void append_csv(char *buffer, unsigned short l);
 PREFIX void append_test_csv(char *buffer, unsigned short l);
+
 PREFIX unsigned short usSelectDevIndex(short i);
+PREFIX unsigned short usSelectDevString(const char *ip_string);
+
 PREFIX unsigned int weigResult(unsigned int i);
 PREFIX unsigned short usGetConnectedDeviceIP(unsigned short i, char *devName);
 PREFIX void RebootCurrentDevice();
