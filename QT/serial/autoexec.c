970f63f5d3f7d2509e7b914d91e81774796b4a8c
Natasha Singh <natasha.iwscope@gmail.com>
2018-01-15 11:40:06 -0500

    Mon Jan 15 11:40:06 EST 2018

--- QT/serial/autoexec.c
+++ QT/serial/autoexec.c
@@ -3,13 +3,13 @@ int main(void)
 {
     int i, j;
     char buffer[100];
-
+    for (j = 0; j < 10; j++)
     for (i = 0; i < 10; i++)
     {
         led_on(i);
-        osDelay(200);
+        osDelay(2);
         led_off(i);
-        osDelay(200);
+        osDelay(2);
     }
 
     return 0;
