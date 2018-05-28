209e56bf0c8b0afd891289cd1b2bfae66638d2ab
Natasha Singh <natasha.iwscope@gmail.com>
2018-05-27 21:57:45 -0400

    Sun May 27 21:57:45 EDT 2018

---
+++ mingw/console_i2c/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+//#include "ext_udp.h"
+
+using namespace std;
+
+
+int main(void)
+{
+    printf("this is console example how to access I2C device from  console\r\n");
+
+
+    return 0;
+}
